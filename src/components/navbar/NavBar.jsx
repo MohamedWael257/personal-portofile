e641218e1ddbe0bfbac410e33792fe8c71e9418a
mohammed12w <116980264+mohammed12w@users.noreply.github.com>
2024-02-14 16:13:39 +0200

    update

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,6 +4,7 @@ import logo from '../../assets/img/logo.svg';
 import navIcon1 from '../../assets/img/nav-icon1.svg';
 import navIcon2 from '../../assets/img/nav-icon2.svg';
 import navIcon3 from '../../assets/img/nav-icon3.svg';
+import navIcon4 from '../../assets/img/nav-icon4.png';
 import { HashLink } from 'react-router-hash-link';
 import './Navbar.css'
 export const NavBar = () => {
@@ -47,9 +48,10 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/mohamed-wael-8b359a27a?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2BTXlWXlpT6%2B92aIaNYtoNg%3D%3D"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://github.com/MohamedWael257"><img src={navIcon4} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>

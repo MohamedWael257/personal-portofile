6c4dac8a9ddd2b3add1513ae704c36248b14e0dd
mohammed12w <116980264+mohammed12w@users.noreply.github.com>
2024-02-14 16:05:38 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/navbar/NavBar';
+import { Banner } from './components/banner/Banner';
+import { Skills } from './components/skills/Skills';
+import { Projects } from './components/projects/Projects';
+import { Contact } from './components/contact/Contact';
+import { Footer } from './components/footer/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
